8278a66c9423e3f72789708950193102ad7cfefc
Steven Cromwell <cromwell7244396@gmail.com>
2025-04-29 23:12:25 -0700

    Update SavingsAccount.py

--- SavingsAccount.py
+++ SavingsAccount.py
@@ -1,23 +1,23 @@
-from BankAccount import BankAccount, InvalidTransactionError
+from BBankAccount import BankAccount, InvalidTransactionError
+
 
 class SavingAccount(BankAccount):
 
-    def __init__(self,name,accountNumber,initialBalance=0,interestRate=0):
-        super().__init__(name,accountNumber,initialBalance)
+    def __init__(self, name, accountNumber, initialBalance=0, interestRate=0):
+        super().__init__(name, accountNumber, initialBalance)
         self.interestRate = interestRate
 
-    def setInterestRate(self,interestRate):
+    def setInterestRate(self, interestRate):
         self.interestRate = interestRate
-    
+
     def getInterestRate(self):
         return self.interestRate
-    
-    def showBalance(self,years):
+
+    def showBalance(self, years):
         self.years = years
         months = years * 12
-        interest = (self.interestRate / 100) + 1
+        interest = (self.interestRate / 100)/12 + 1
         for i in range(months):
             self.balance *= interest
         return self.balance
-
 
